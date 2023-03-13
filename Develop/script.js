54024aba9637f5ca2cf2fc27c011169098a8f4a7
Strait2thePoint <122751055+strait2thepoint@users.noreply.github.com>
2023-03-13 06:39:04 -0400

    added strArr and for loop

--- Develop/script.js
+++ Develop/script.js
@@ -20,16 +20,17 @@ function displayTime (){
 displayTime();
 
 // //working on my own stuff here:  need loop for rest of hours
-
 let saveBtn = document.querySelector('.saveBtn');  
 let description = document.querySelector('.description'); 
 
 saveBtn.addEventListener('click', addToLocalStorage);
 
-description.value = localStorage.getItem('content');
+const strArr = ['hour-6', 'hour-7','hour-8','hour-9','hour-10','hour-11','hour-12','hour-13','hour-14','hour-15','hour-16','hour-17','hour-18'];
+
+for(let i = 0; i < strArr.length; i++){
+ description.value = localStorage.getItem('content');
 
 function addToLocalStorage(){
 let content = document.querySelector('.hour-6');
 localStorage.setItem('content', description.value);  
-}
-
+} }
